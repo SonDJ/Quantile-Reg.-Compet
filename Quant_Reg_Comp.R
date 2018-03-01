d52094700ac6f781caa1233b5429119330b3b002
SonDJ <36252020+SonDJ@users.noreply.github.com>
2018-03-02 00:14:14 +0900

    Update Quant_Reg_Comp.R

--- Quant_Reg_Comp.R
+++ Quant_Reg_Comp.R
@@ -1,4 +1,4 @@
-library(cmprskQR) ; library(survival)
+library(cmprskQR) ; library(survival) ; library(testit) ; library(nleqslv)
 
 cause_sampling_func=function(z){
   sample_vec=c()
@@ -13,10 +13,10 @@ sampling_func=function(dist, status, z1, z2){
   time_vec=c()
   
   for(i in 1:length(status)){
-  if(dist=='normal') time_vec=ifelse(status==1, exp(-rep(1,length(status))+z1+z2+qnorm(unif, 0, 1)), exp(-rep(1,length(status))+z1-z2+qnorm(unif, 0, 1)))
-  else if(dist=='logistic') time_vec=ifelse(status==1, exp(-rep(1,length(status))+z1+z2+qlogis(unif, 0, 1)), exp(-rep(1,length(status))+z1-z2+qlogis(unif, 0, 1)))
-  else if(dist=='cauchy') time_vec=ifelse(status==1, exp(-rep(1,length(status))+z1+z2+qcauchy(unif, 0, 1)), exp(-rep(1,length(status))+z1-z2+qcauchy(unif, 0, 1)))
-  else if(dist=='weibull') time_vec=ifelse(status==1, exp(-rep(1,length(status))+z1+z2+qweibull(unif, 0.5, 1)), exp(-rep(1,length(status))+z1-z2+qweibull(unif, 0.5, 1)))
+    if(dist=='normal') time_vec=ifelse(status==1, exp(-rep(1,length(status))+z1+z2+qnorm(unif, 0, 1)), exp(-rep(1,length(status))+z1-z2+qnorm(unif, 0, 1)))
+    else if(dist=='logistic') time_vec=ifelse(status==1, exp(-rep(1,length(status))+z1+z2+qlogis(unif, 0, 1)), exp(-rep(1,length(status))+z1-z2+qlogis(unif, 0, 1)))
+    else if(dist=='cauchy') time_vec=ifelse(status==1, exp(-rep(1,length(status))+z1+z2+qcauchy(unif, 0, 1)), exp(-rep(1,length(status))+z1-z2+qcauchy(unif, 0, 1)))
+    else if(dist=='weibull') time_vec=ifelse(status==1, exp(-rep(1,length(status))+z1+z2+qweibull(unif, 0.5, 1)), exp(-rep(1,length(status))+z1-z2+qweibull(unif, 0.5, 1)))
   }
   return(time_vec)
 }
@@ -63,23 +63,28 @@ smooth.est.eq=function(tau, n, obs, status, covariate, beta, sigma){
 }
 
 NewtonRaphson=function(taus, ns, obss, statuss, covariates, betas){
-  sols=as.matrix(betas) ; cov=1/ns*diag(rep(1, ncol(covariates)), ncol(covariates)) ; i=1
+  sols=list(betas) ; cov=1/ns*diag(rep(1, ncol(covariates)), ncol(covariates)) ; i=1
+  f=function(x) class(try(solve(x),silent=T))=="matrix"
+  
   repeat{
-    new.beta=sols[,i]-solve(A(n = ns, obs = obss, status = statuss, covariate = covariates, beta = sols[,i], sigma = cov))%*%as.matrix(smooth.est.eq(tau = taus, n = ns, obs = obss, status = statuss, covariate = covariates, beta = sols[,i], sigma = cov))
-    sols=cbind(sols, new.beta)
-    new.sigma=1/ns*solve(A(n = ns, obs = obss, status = statuss, covariate = covariates, beta = sols[,i], sigma = cov))%*%gamma(tau = taus, n = ns, obs = obss, status = statuss, covariate = covariates, beta = sols[,i])%*%solve(A(n = ns, obs = obss, status = statuss, covariate = covariates, beta = sols[,i], sigma = cov))
-    cov=new.sigma
-    i=i+1
-    if(all(sapply(abs(sols[,i]-sols[,i-1]), FUN=function(j) {I(j<10^-5)}))) break
+    if(f(A(n = ns, obs = obss, status = statuss, covariate = covariates, beta = as.matrix(sols[[i]]), sigma = cov))==FALSE|any(is.nan(smooth.est.eq(tau = taus, n = ns, obs = obss, status = statuss, covariate = covariates, beta = as.matrix(sols[[i]]), sigma = cov)))) {sols[[i]]=NA ; break
+    } else {
+      new.beta=as.matrix(sols[[i]])-solve(A(n = ns, obs = obss, status = statuss, covariate = covariates, beta = as.matrix(sols[[i]]), sigma = cov))%*%as.matrix(smooth.est.eq(tau = taus, n = ns, obs = obss, status = statuss, covariate = covariates, beta = as.matrix(sols[[i]]), sigma = cov))
+      new.sigma=1/ns*solve(A(n = ns, obs = obss, status = statuss, covariate = covariates, beta = as.matrix(sols[[i]]), sigma = cov))%*%gamma(tau = taus, n = ns, obs = obss, status = statuss, covariate = covariates, beta = as.matrix(sols[[i]]))%*%solve(A(n = ns, obs = obss, status = statuss, covariate = covariates, beta = as.matrix(sols[[i]]), sigma = cov))
+      cov=new.sigma
+      i=i+1
+      sols[[i]]=as.vector(new.beta)
+      if(all(sapply(abs(sols[[i]]-sols[[i-1]]), FUN=function(j) {I(j<10^-3)}))) break
+    }
   }
-  return(list(sols[,ncol(sols)], cov))
+  if(any(unlist(lapply(sols, is.null)))) NA else list(sols[[length(sols)]], cov)
 }
 
 #m-repetitions of N-sample simulation with given Tau and specific distribution
 
 simulation_function=function(m, N, Dist, L, Tau){
-  reg.est=list() ; cov.est=list()
-  for(i in 1:m){
+  reg.est=list() ; cov.est=list() ; i=1
+  repeat{
     Z1=runif(n = N, min = -1, max = 1) ; Z2=rbinom(n = N, size = 1, prob = 0.5)
     Eps=cause_sampling_func(Z2)
     time=sampling_func(dist = Dist, status = Eps, z1 = Z1, z2 = Z2)
@@ -89,39 +94,44 @@ simulation_function=function(m, N, Dist, L, Tau){
     pfcmp=crrQR(ftime = log(Obs), fstatus = Eps, X = model.matrix(~Z1+Z2)[,-1], tau.range = c(Tau, Tau))
     a=NewtonRaphson(taus = Tau, ns = N, obss = Obs, statuss = Eps, covariates = cbind(rep(1,N), Z1, Z2), betas = as.vector(pfcmp$beta.seq))
     reg.est[[i]]=a[[1]] ; cov.est[[i]]=a[[2]]
+    i=i+1
+    if(length(reg.est)==m) break
   }
-  return(list(1/m*Reduce('+', reg.est), 1/m*Reduce('+', cov.est)))
+  return(list(1/length(reg.est)*Reduce('+', reg.est), 1/length(reg.est)*Reduce('+', cov.est)))
 }
 
-normal.model.300=simulation_function(m = 100, N = 300, Dist = 'normal', L = 4.6, Tau = 0.2)
+normal.model.300=simulation_function(m = 5, N = 300, Dist = 'normal', L = 4.6, Tau = 0.2)
 
 #no consideration of covariance matrix
 
-Naive.NewtonRaphson=function(taus, ns, obss, statuss, covariates, betas){
-  sols=as.matrix(betas) ; cov=1/ns*diag(rep(1, ncol(covariates)), ncol(covariates)) ; i=1
+Naive_simulation_function=function(m, N, Dist, L, Tau){
+  cov.est=diag(rep(1/N, 3), 3) ; i=1 ; m.sol=list()
   repeat{
-    new.beta=sols[,i]-solve(A(n = ns, obs = obss, status = statuss, covariate = covariates, beta = sols[,i], sigma = cov))%*%as.matrix(smooth.est.eq(tau = taus, n = ns, obs = obss, status = statuss, covariate = covariates, beta = sols[,i], sigma = cov))
-    sols=cbind(sols, new.beta)
+    Z1=runif(n = N, min = -1, max = 1) ; Z2=rbinom(n = N, size = 1, prob = 0.5)
+    Eps=cause_sampling_func(Z2)
+    time=sampling_func(dist = Dist, status = Eps, z1 = Z1, z2 = Z2)
+    cens=runif(n = N, min = 0, max = L)
+    Obs=ifelse(time>cens, cens, time)
+    Eps=ifelse(Obs==cens, 0, Eps)
+    pfcmp=crrQR(ftime = log(Obs), fstatus = Eps, X = model.matrix(~Z1+Z2)[,-1], tau.range = c(Tau, Tau))
+    
+    beta.smooth.est.eq=function(beta){
+      covariate=cbind(rep(1, N), Z1, Z2); sigma=cov.est
+      km.cens=survfit(Surv(time = Obs, event = ifelse(Eps==0,1,0))~1)$surv
+      if(km.cens[N]==0) km.cens[N]=km.cens[N-1] else km.cens[N]=km.cens[N]
+      sum.list=list()
+      for(i in 1:N){
+        sum.list[[i]]=covariate[i,]*as.numeric(ifelse(Eps[i]==1, 1, 0)/km.cens[i]*pnorm(-(log(Obs[i])-(covariate%*%beta)[i])/sqrt(as.numeric(t(covariate[i,])%*%sigma%*%covariate[i,])), 0, 1)-Tau)
+      }
+      return(1/N*Reduce('+', sum.list))
+    }
+    
+    m.sol[[i]]=nleqslv(x = as.vector(pfcmp$beta.seq), fn = beta.smooth.est.eq, method = c("Newton"))$x
     i=i+1
-    if(all(sapply(abs(sols[,i]-sols[,i-1]), FUN=function(j) {I(j<1/(10^5))}))) break
+    if(length(m.sol)==m) break
   }
-  return(list(sols[,ncol(sols)], cov))
+  return(list(1/length(m.sol)*Reduce('+', m.sol), cov.est))
 }
 
-naive.n.300=Naive.NewtonRaphson(taus = Tau, ns = N, obss = obs, statuss = eps, covariates = cbind(rep(1,N), Z1, Z2), betas = as.vector(pfcmp$beta.seq))
-
-#Using Equation Solver
-
-beta.smooth.est.eq=function(beta){
-  tau=0.2; n=300; obs=n.obs.300; status=eps.300; covariate=cbind(rep(1,300),z1.300,z2.300); sigma=diag(rep(1/300,3),3)
-  km.cens=survfit(Surv(time = obs, event = ifelse(status==0,1,0))~1)$surv
-  sum.list=list()
-  for(i in 1:n){
-    sum.list[[i]]=covariate[i,]*as.numeric(ifelse(status[i]==1, 1, 0)/km.cens[i]*pnorm(-(log(obs[i])-(covariate%*%beta)[i])/sqrt(t(covariate[i,])%*%sigma%*%covariate[i,]), 0, 1)-tau)
-  }
-  return(1/n*Reduce('+', sum.list))
-}
-
-library(rootSolve)
-
-m=multiroot(f = beta.smooth.est.eq, start = c(-1.4,1,0.75))
+naive.normal.model.300=Naive_simulation_function(m = 100, N = 300, Dist = 'normal', L = 4.6, Tau = 0.2)
+naive.logistic.model.300=Naive_simulation_function(m = 100, N = 300, Dist = 'logistic', L = 15, Tau = 0.2)

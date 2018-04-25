2ce3b83096e7229368523b73b125d2740482f99b
SonDJ <36252020+SonDJ@users.noreply.github.com>
2018-04-26 01:13:18 +0900

    Update Quant_Reg_Comp.R

--- Quant_Reg_Comp.R
+++ Quant_Reg_Comp.R
@@ -1,4 +1,6 @@
 library(cmprskQR) ; library(survival) ; library(nleqslv)
+na.omit.list=function(y) { return(y[!sapply(y, function(x) all(is.na(x)))]) }
+
 cause_sampling_func=function(z){
   sample_vec=c()
   for(i in 1:length(z)){
@@ -46,91 +48,57 @@ gamma=function(tau, n, obs, status, covariate, beta){
   return(1/n*Reduce('+', sum.mat.1)-1/n*Reduce('+', l))
 }
 
-A=function(n, obs, status, covariate, beta, sigma){
+A=function(n, obs, status, covariate, beta, sigma, CCH=F){
   SF=survfit(formula = Surv(time = obs, event = ifelse(status==0, 1, 0))~1)
   km.cens=SF$surv
   obs.per=c()
   for(i in 1:n){
     obs.per[i]=which(obs==SF$time[i])
   }
+  pn=nrow(complete.cases(covariate))/n
   sum.list=list()
   for(i in 1:n){
-    sum.list[[i]]=as.numeric(ifelse(status[which(obs==SF$time[i])]==1, 1/km.cens[i], 0)*dnorm(-(log(obs[obs.per[i]])-(covariate%*%beta)[obs.per[i]])/sqrt(as.numeric(t(covariate[obs.per[i],])%*%sigma%*%covariate[obs.per[i],])), 0, 1))/as.numeric(sqrt(t(covariate[obs.per[i],])%*%sigma%*%covariate[obs.per[i],]))*outer(covariate[obs.per[i],], covariate[obs.per[i],])
+    sum.list[[i]]=ifelse(CCH==T, ifelse(status[obs.per[i]]==1, 1, ifelse(any(is.na(covariate[obs.per[i],]))==F, 1, 0)/pn), 1)*as.numeric(ifelse(status[which(obs==SF$time[i])]==1, 1/km.cens[i], 0)*dnorm(-(log(obs[obs.per[i]])-(covariate%*%beta)[obs.per[i]])/sqrt(as.numeric(t(covariate[obs.per[i],])%*%sigma%*%covariate[obs.per[i],])), 0, 1))/as.numeric(sqrt(t(covariate[obs.per[i],])%*%sigma%*%covariate[obs.per[i],]))*outer(covariate[obs.per[i],], covariate[obs.per[i],])
   }
-  return(1/n*Reduce('+', sum.list))
+  sum.list=na.omit.list(sum.list)
+  return(1/length(sum.list)*Reduce('+', sum.list))
 }
 
-smooth.est.eq=function(beta, tau, n, obs, status, covariate, sigma){
+smooth.est.eq=function(beta, tau, n, obs, status, covariate, sigma, CCH=F){
   SF=survfit(formula = Surv(time = obs, event = ifelse(status==0, 1, 0))~1)
   km.cens=SF$surv
   obs.per=match(x = SF$time, table = obs)
+  pn=nrow(complete.cases(covariate))/n
   sum.list=list()
   for(i in 1:n){
     if(status[which(obs==SF$time[i])]==1){
-      sum.list[[i]]=as.vector(covariate[obs.per[i],])*as.numeric(ifelse(status[which(obs==SF$time[i])]==1, 1/km.cens[i], 0)*pnorm(-(log(obs[obs.per[i]])-(covariate%*%beta)[obs.per[i]])/sqrt(as.numeric(t(covariate[obs.per[i],])%*%sigma%*%covariate[obs.per[i],])), 0, 1)-tau)
+      sum.list[[i]]=ifelse(CCH==T, ifelse(status[obs.per[i]]==1, 1, ifelse(any(is.na(covariate[obs.per[i],]))==F, 1, 0)/pn), 1)*as.vector(covariate[obs.per[i],])*as.numeric(ifelse(status[which(obs==SF$time[i])]==1, 1/km.cens[i], 0)*pnorm(-(log(obs[obs.per[i]])-(covariate%*%beta)[obs.per[i]])/sqrt(as.numeric(t(covariate[obs.per[i],])%*%sigma%*%covariate[obs.per[i],])), 0, 1)-tau)
     }
     else{
-      sum.list[[i]]=-covariate[obs.per[i],]*tau
+      sum.list[[i]]=-ifelse(CCH==T, ifelse(status[obs.per[i]]==1, 1, ifelse(any(is.na(covariate[obs.per[i],]))==F, 1, 0)/pn), 1)*covariate[obs.per[i],]*tau
     }
   }
-  return(1/n*Reduce('+', sum.list))
+  sum.list=na.omit.list(sum.list)
+  return(1/length(sum.list)*Reduce('+', sum.list))
 }
-
-NewtonRaphson=function(taus, ns, obss, statuss, covariates, betas){
-  sols=list(betas) ; cov=1/ns*diag(rep(1, ncol(covariates)), ncol(covariates)) ; i=1
-  f=function(x) class(try(solve(x),silent=T))=="matrix"
-  
-  repeat{
-    if(f(A(n = ns, obs = obss, status = statuss, covariate = covariates, beta = as.matrix(sols[[i]]), sigma = cov))==FALSE|any(is.nan(smooth.est.eq(tau = taus, n = ns, obs = obss, status = statuss, covariate = covariates, beta = as.matrix(sols[[i]]), sigma = cov)))) {sols[[i]]=NA ; break
-    } else {
-      new.beta=as.matrix(sols[[i]])-solve(A(n = ns, obs = obss, status = statuss, covariate = covariates, beta = as.matrix(sols[[i]]), sigma = cov))%*%as.matrix(smooth.est.eq(tau = taus, n = ns, obs = obss, status = statuss, covariate = covariates, beta = as.matrix(sols[[i]]), sigma = cov))
-      new.sigma=1/ns*solve(A(n = ns, obs = obss, status = statuss, covariate = covariates, beta = as.matrix(sols[[i]]), sigma = cov))%*%gamma(tau = taus, n = ns, obs = obss, status = statuss, covariate = covariates, beta = as.matrix(sols[[i]]))%*%solve(A(n = ns, obs = obss, status = statuss, covariate = covariates, beta = as.matrix(sols[[i]]), sigma = cov))
-      cov=new.sigma
-      i=i+1
-      sols[[i]]=as.vector(new.beta)
-      if(all(sapply(abs(sols[[i]]-sols[[i-1]]), FUN=function(j) {I(j<10^-3)}))) break
-    }
-  }
-  if(any(unlist(lapply(sols, is.null)))) NA else list(sols[[length(sols)]], cov)
-}
-
-#m-repetitions of N-sample simulation with given Tau and specific distribution
-
-simulation_function=function(m, N, Dist, L, Tau){
-  reg.est=list() ; cov.est=list() ; i=1
-  repeat{
-    Z1=runif(n = N, min = -1, max = 1) ; Z2=rbinom(n = N, size = 1, prob = 0.5)
-    Eps=cause_sampling_func(Z2)
-    time=sampling_func(dist = Dist, status = Eps, z1 = Z1, z2 = Z2)
-    cens=runif(n = N, min = 0, max = L)
-    Obs=ifelse(time>cens, cens, time)
-    Eps=ifelse(Obs==cens, 0, Eps)
-    pfcmp=suppressMessages(crrQR(ftime = log(Obs), fstatus = Eps, X = model.matrix(~Z1+Z2)[,-1], tau.range = c(Tau, Tau)))
-    a=NewtonRaphson(taus = Tau, ns = N, obss = Obs, statuss = Eps, covariates = cbind(rep(1,N), Z1, Z2), betas = as.vector(pfcmp$beta.seq))
-    reg.est[[i]]=a[[1]] ; cov.est[[i]]=a[[2]]
-    i=i+1
-    if(length(reg.est)==m) break
-  }
-  return(list(1/length(reg.est)*Reduce('+', reg.est), 1/length(reg.est)*Reduce('+', cov.est)))
-}
-
-normal.model.300=simulation_function(m = 100, N = 300, Dist = 'normal', L = 4.6, Tau = 0.2)
 
 #MB method
-boot.smooth.est.eq=function(tau, n, obs, status, covariate, beta, sigma, eta){
+boot.smooth.est.eq=function(tau, n, obs, status, covariate, beta, sigma, eta, CCH=F){
   SF=survfit(formula = Surv(time = obs, event = ifelse(status==0, 1, 0))~1)
   km.cens=SF$surv
   obs.per=match(x = SF$time, table = obs)
+  pn=nrow(complete.cases(covariate))/n
   sum.list=list()
   for(i in 1:n){
     if(status[which(obs==SF$time[i])]==1){
-      sum.list[[i]]=eta[obs.per[i]]*covariate[obs.per[i],]*as.numeric(ifelse(status[obs.per[i]]==1, 1/km.cens[i], 0)*pnorm(-(log(obs[obs.per[i]])-(covariate%*%beta)[obs.per[i]])/sqrt(as.numeric(t(covariate[obs.per[i],])%*%sigma%*%covariate[obs.per[i],])), 0, 1)-tau)
+      sum.list[[i]]=ifelse(CCH==T, ifelse(status[obs.per[i]]==1, 1, ifelse(any(is.na(covariate[obs.per[i],]))==F, 1, 0)/pn), 1)*eta[obs.per[i]]*covariate[obs.per[i],]*as.numeric(ifelse(status[obs.per[i]]==1, 1/km.cens[i], 0)*pnorm(-(log(obs[obs.per[i]])-(covariate%*%beta)[obs.per[i]])/sqrt(as.numeric(t(covariate[obs.per[i],])%*%sigma%*%covariate[obs.per[i],])), 0, 1)-tau)
     }
     else{
-      sum.list[[i]]=-eta[i]*covariate[obs.per[i],]*tau
+      sum.list[[i]]=-ifelse(CCH==T, ifelse(status[obs.per[i]]==1, 1, ifelse(any(is.na(covariate[obs.per[i],]))==F, 1, 0)/pn), 1)*eta[i]*covariate[obs.per[i],]*tau
     }
   }
-  return(1/n*Reduce('+', sum.list))
+  sum.list=na.omit.list(sum.list)
+  return(1/length(sum.list)*Reduce('+', sum.list))
 }
 
 MB_simulation_function=function(m, B, N, Dist, L, Tau){
@@ -171,8 +139,17 @@ MB_simulation_function=function(m, B, N, Dist, L, Tau){
 set.seed(300)
 naive.normal.model.300=MB_simulation_function(m = 50, B = 100, N = 300, Dist = 'normal', L = 4.6, Tau = 0.2)
 
+#Case-cohort design
+CCHD=function(rate, data, status){
+  location=which(status==1)
+  sub.coh.ind=sample(x = 1:nrow(data), size = floor(nrow(data)*rate), replace = F)
+  case.coh.ind=c(sub.coh.ind, setdiff(location, sub.coh.ind))
+  data[setdiff(x = 1:nrow(data), y = case.coh.ind), ]=rep(NA, ncol(data))
+  return(data)
+}
+
 #IS-MB method
-ISMB_simulation_function=function(m, B, N, Dist, L, Tau){
+ISMB_simulation_function=function(m, B, N, Dist, L, Tau, Cohort=F){
   cov.est=diag(rep(1/N, 3), 3) ; m.sol=list() ; boot.cov=list() ; i=1
   repeat{
     Z1=runif(n = N, min = -1, max = 1) ; Z2=rbinom(n = N, size = 1, prob = 0.5)
@@ -181,6 +158,7 @@ ISMB_simulation_function=function(m, B, N, Dist, L, Tau){
     cens=runif(n = N, min = 0, max = L)
     Obs=ifelse(time>cens, cens, time)
     Eps=ifelse(Obs==cens, 0, Eps)
+    COVAR=if(Cohort==T) CCHD(rate = 0.1, data = cbind(rep(1, N), Z1, Z2), status = Eps) else cbind(rep(1, N), Z1, Z2)
     
     if(length(survfit(formula = Surv(time = Obs, event = ifelse(Eps==0, 1, 0))~1)$time)<N) {
       m.sol[[i]]=NULL
@@ -189,13 +167,13 @@ ISMB_simulation_function=function(m, B, N, Dist, L, Tau){
     
     else if(length(survfit(formula = Surv(time = Obs, event = ifelse(Eps==0, 1, 0))~1)$time)==N){
       pfcmp=suppressMessages(crrQR(ftime = log(Obs), fstatus = Eps, X = model.matrix(~Z1+Z2)[,-1], tau.range = c(Tau, Tau)))
-      m.sol[[i]]=nleqslv(x = as.vector(pfcmp$beta.seq), fn = smooth.est.eq, method = c("Newton"), tau = Tau, n = N, obs = Obs, status = Eps, covariate = cbind(rep(1, N), Z1, Z2), sigma = cov.est)$x
+      m.sol[[i]]=nleqslv(x = as.vector(pfcmp$beta.seq), fn = smooth.est.eq, method = c("Newton"), tau = Tau, n = N, obs = Obs, status = Eps, covariate = COVAR, sigma = cov.est, CCH = Cohort)$x
       
       boot.list=list()
       
       for(j in 1:B){
         Eta=rexp(n = N, rate = 1)
-        boot.list[[j]]=boot.smooth.est.eq(tau = Tau, n = N, obs = Obs, status = Eps, covariate = cbind(rep(1, N), Z1, Z2), beta = m.sol[[i]], sigma = cov.est, eta = Eta)
+        boot.list[[j]]=boot.smooth.est.eq(tau = Tau, n = N, obs = Obs, status = Eps, covariate = COVAR, beta = m.sol[[i]], sigma = cov.est, eta = Eta, CCH = Cohort)
       }
       
       boot.bar=as.vector(1/length(boot.list)*Reduce('+', boot.list))
@@ -217,4 +195,4 @@ ISMB_simulation_function=function(m, B, N, Dist, L, Tau){
 }
 
 set.seed(300)
-ISMB.normal.model.300=ISMB_simulation_function(m = 300, B = 300, N = 300, Dist = 'normal', L = 4.2, Tau = 0.2)
+ISMB.normal.model.300=ISMB_simulation_function(m = 100, B = 100, N = 300, Dist = 'normal', L = 7, Tau = 0.2, Cohort = T)
